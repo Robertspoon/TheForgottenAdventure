5edbe20c4eb6cb7d95fe365785144ba83db4a675
Robert Atkinson <robertthemcjug@gmail.com>
2023-05-01 15:41:56 -0300

    Began to construct the game

---
+++ TheForgottenAdventure/Map.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForgottenAdventure
+{
+    internal class Map
+    { 
+        //fields
+        private string[] map = System.IO.File.ReadAllLines("Map.txt");
+
+    }
+}
